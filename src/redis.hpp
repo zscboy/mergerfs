4c19eacd7dfba6f4bcd9ad09ba87967628634204
riguang <zscboy@gmail.com>
2022-06-22 15:49:26 +0800

    redis

--- src/redis.hpp
+++ src/redis.hpp
@@ -17,8 +17,8 @@ class Redis
         static sw::redis::OptionalString get(string key);
         static bool hset(string hash, string field, string value);
         static sw::redis::OptionalString hget(string hash, string field);
-        
+
     private:
-        static sw::redis::Redis *redis = NULL; 
+        static sw::redis::Redis *redis; 
 
 };
