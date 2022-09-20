fd1e7d06e03175a39b866c3ef835981e1cbcce1c
riguang <zscboy@gmail.com>
2022-09-20 15:43:55 +0800

    parse cominedir params

--- src/policy_redis.cpp
+++ src/policy_redis.cpp
@@ -130,9 +130,19 @@ namespace redis
          const char           *fusepath_,
          StrVec               *paths_)
   {
-    const string sealedpath = "/sealed";
-    const string cachepath = "/cache";
+    // const string sealedpath = "/sealed";
+    // const string cachepath = "/cache";
     Config::Read cfg;
+
+    StrVec combinedirs
+    str::split(cfg->combinedirs,':',&combinedirs);
+    if (combinedirs.size() < 2) {
+       std::cout << "create parse cominedir error, size:" << combinedirs.size() << std::endl;
+       return -1;
+    }
+    
+    string cachepath = combinedirs[0]
+    string sealedpath = combinedirs[1]
 
     string searchpath;
     string fusedirpath = fs::path::dirname(fusepath_);
